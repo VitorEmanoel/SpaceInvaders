c0adf8e989e2b5f87d5d8a558e5b041c6d28bee9
VitorEmanoel <vitoremanoelamorim@hotmail.com>
2018-08-03 12:41:11 -0300

    Added assets and minor modifications

--- game/GameEngine.js
+++ game/GameEngine.js
@@ -1,2 +1,22 @@
-const Entity = require("./Entity.js");
-const Game = require("./Game.js");
+var Game = {
+    start : function(){
+        this.canvas = document.getElementById("Game");
+    }
+};
+
+function Entity(height, width, image){
+
+    this.image = new Image(width, height);
+    this.image.src = image;
+    this.image.style.position = "absolute";
+    this.height = height;
+    this.width = width;
+    this.context = Game.canvas.getContext("2d");
+    this.x = 0;
+    this.y = 0;
+
+}
+
+function startGame() {
+    Game.start();
+}
